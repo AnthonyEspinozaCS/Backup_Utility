77bc4d9bb53fb5ae3dff7389d863ba50bc47d588
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2024-05-10 20:30:35 -0700

    Added error checking and displayed the error code in the case of failure.

--- backup.bat
+++ backup.bat
@@ -1,5 +1,11 @@
 @ECHO OFF
 set MMDDYYYY=%DATE:~4,2%-%DATE:~7,2%-%DATE:~12,2%
 ECHO Beginning Backup of %MMDDYYYY%
-robocopy C:\Users\Oblivion\Documents\Random C:\Users\Oblivion\Documents\Backup_%MMDDYYYY% /s /NFL /NDL /NJH /NJS /nc /ns /np
+robocopy C:\Users\Oblivion\Documents\Random C:\Users\Oblivion\Documents\git_repos\Backup_Utility\Backup_%MMDDYYYY% /s /NFL /NDL /NJH /NJS /nc /ns /np
+set exitcode=%errorlevel%
+if %exitcode% equ 0 (
+    ECHO Backup completed successfully.
+) else (
+    ECHO Backup failed with error code %errorcode%
+)
 PAUSE
