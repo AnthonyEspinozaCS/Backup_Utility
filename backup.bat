a9ebe5d57be4c8789b243a12ca75e8c998149d4b
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2024-06-06 11:00:42 -0700

    Rearranged folder hierarchy.

--- backup.bat
+++ backup.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 set MMDDYYYY=%DATE:~4,2%-%DATE:~7,2%-%DATE:~12,2%
 ECHO Beginning Backup of %MMDDYYYY%
-robocopy C:\Users\Oblivion\Documents\Random C:\Users\Oblivion\Documents\git_repos\Backup_Utility\Backup_%MMDDYYYY% /s /NFL /NDL /NJH /NJS /nc /ns /np
+
+robocopy C:\Users\Oblivion\Documents\Random C:\Users\Oblivion\Documents\git_repos\Backup_Utility\backups\Backup_%MMDDYYYY% /s /NFL /NDL /NJH /NJS /nc /ns /np
 set exitcode=%errorlevel%
+
 if %errorlevel% equ 1 (
     ECHO Backup completed successfully.
 ) else (
