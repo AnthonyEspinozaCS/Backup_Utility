cade056b462387f1f752012c87cd4ff6fd09c164
AnthonyEspinozaCS <aespinoza.cs@gmail.com>
2024-06-06 10:47:07 -0700

    Exit terminal window automatically.

--- backup.bat
+++ backup.bat
@@ -3,9 +3,10 @@ set MMDDYYYY=%DATE:~4,2%-%DATE:~7,2%-%DATE:~12,2%
 ECHO Beginning Backup of %MMDDYYYY%
 robocopy C:\Users\Oblivion\Documents\Random C:\Users\Oblivion\Documents\git_repos\Backup_Utility\Backup_%MMDDYYYY% /s /NFL /NDL /NJH /NJS /nc /ns /np
 set exitcode=%errorlevel%
-if %exitcode% equ 0 (
+if %errorlevel% equ 1 (
     ECHO Backup completed successfully.
 ) else (
-    ECHO Backup failed with error code %errorcode%
+    ECHO Backup failed with error code %errorlevel%
 )
-PAUSE
+timeout /t 10
+exit
